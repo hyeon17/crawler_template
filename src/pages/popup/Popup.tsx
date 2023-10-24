5857bd1523e410eca965f40dd6936f5e3a43d538
KEN <83224463+hyeon17@users.noreply.github.com>
2023-10-24 14:33:03 +0900

    Feat: cheerio 테스트

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,32 +1,49 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
 import '@pages/popup/Popup.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
+import axios from 'axios';
+import { load } from 'cheerio';
+// import { launch } from 'puppeteer';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
+  const getUrl = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      if (tabs && tabs[0] && tabs[0].url) {
+        renderHTML(tabs[0].url);
+      } else {
+        console.log('URL not available');
+      }
+    });
+  };
+
+  const renderHTML = async (url: string) => {
+    const res = await axios.get(url);
+    const $ = load(res.data);
+    // const body = $('body');
+    // const link = '//item.taobao.com/item.htm?id=';
+    const data = [];
+    const originProductName = $('#J_Title > .tb-main-title').text().trim();
+    const scopePrice = $('#J_StrPrice > em.tb-rmb-num').text();
+    const salePrice = $('#J_PromoHd > div > strong > em.tb-rmb-num').text();
+    const originCity = $('#J-From').text().trim();
+    const mainImageSelector = $('#J_UlThumb > li > div > a > img').toArray();
+    const detailImageSelector = $('#J_DivItemDesc > p').find('img').toArray();
+    const optionsArray = $('#J_isku > div > dl > dd > ul > ').toArray();
+    data.push({
+      originProductName,
+      scopePrice,
+      salePrice,
+      originCity,
+      mainImageSelector,
+      detailImageSelector,
+      optionsArray,
+    });
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-lime-400">
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React!
-        </a>
-        <button
-          style={{
-            color: theme === 'light' ? '#fff' : '#000',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme: [{theme}]
-        </button>
-      </header>
+      <button onClick={getUrl}>Hello</button>
     </div>
   );
 };
