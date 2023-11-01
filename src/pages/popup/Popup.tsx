fd7ef5098623dbf47638a796d5805380d21dd77f
KEN <83224463+hyeon17@users.noreply.github.com>
2023-11-01 12:00:33 +0900

    Feat: 정적 크롤링 기능 추가

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,27 +1,41 @@
 import '@pages/popup/Popup.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-// import axios from 'axios';
-import {renderHTML} from '../../../server'
+import axios from 'axios';
+import { load } from 'cheerio';
+
 const Popup = () => {
   const getUrl = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const regex = /id=(\d+)/;
       if (tabs && tabs[0] && tabs[0].url) {
-        const match = tabs[0].url.match(regex);
-        const id = match[1];
-        const res = renderHTML(id);
-        console.log(res);
+        renderHTML(tabs[0].url);
       } else {
         console.log('URL not available');
       }
     });
   };
 
-  // const sendRequestToServer = async (url) => {
-  //   const res = await axios.post('http://localhost:3000', {url});
-  //   console.log(res);
-  // };
+  const renderHTML = async (url: string) => {
+    const res = await axios.get(url);
+    const $ = load(res.data);
+    const data = {
+      originProductName: '',
+      scopePrice: '',
+      salePrice: '',
+      originCity: '',
+      mainImageSelector: [],
+      detailImageSelector: [],
+      optionsArray: [],
+    };
+    data.originProductName = $('#J_Title > .tb-main-title').text().trim();
+    data.scopePrice = $('#J_StrPrice > em.tb-rmb-num').text();
+    data.salePrice = $('#J_PromoHd > div > strong > em.tb-rmb-num').text();
+    data.originCity = $('#J-From').text().trim();
+    data.mainImageSelector = $('#J_UlThumb > li > div > a > img').toArray();
+    data.detailImageSelector = $('#J_DivItemDesc > p').find('img').toArray();
+    data.optionsArray = $('#J_isku > div > dl > dd > ul > ').toArray();
+    console.log(data);
+  };
 
   return (
     <div className="App">
